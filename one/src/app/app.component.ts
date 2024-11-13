d2935ae28c65224ab6fccb95449ea25f59a027df
Sxnic <102521739+sxpersxnic@users.noreply.github.com>
2024-11-13 10:21:07 +0000

    Init

---
+++ one/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  template: `
+    <h1>One</h1>
+  `,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'default';
+}
